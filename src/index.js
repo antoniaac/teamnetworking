b045c9b13e479a64a0b6ca22ec8a9b19abef2c62
Ciont Antonia <ciontantonia@yahoo.com>
2023-03-16 19:17:27 +0200

    updated the project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+let allTeams = [];
+let editId;
 fetch("http://localhost:3000/teams-json", {
   method: "GET",
   headers: {
@@ -6,61 +8,113 @@ fetch("http://localhost:3000/teams-json", {
 })
   .then(r => r.json())
   .then(teams => {
+    //window.teams = teams;
+    allTeams = teams;
+    console.info(teams);
     displayTeams(teams);
   });
-function displayTeams(teams) {
-  const teamsHTML = teams.map(
-    team =>
-      `<tr>
-      <td>${team.promotion}</td>
-      <td>${team.members}</td>
-      <td>${team.name}</td>
-      <td>${team.url}</td>
-      <td>
-        <a>❌</a>
-      </td>
-    </tr>`
-  );
-  document.querySelector("#teams tbody").innerHTML = teamsHTML.join("");
-}
-
-function onSumbit(e) {
-  e.preventDefault();
-  fetch("http://localhost:3000/teams-json/create", {
+function createTeamRequest(team) {
+  return fetch("http://localhost:3000/teams-json/create", {
     method: "POST",
     headers: {
       "Content-Type": "application/json"
     },
-    body: JSON.stringify({
-      promotion: document.getElementById("promotion").value,
-      members: document.getElementById("Members").value,
-      name: document.getElementById("name").value,
-      url: document.getElementById("url").value
-    })
-  })
-    .then(r => r.json())
-    .then(status => {
-      console.warn("status", status.success, status.id);
-      if (status.success) {
-        window.location.reload();
-      }
-    });
+    body: JSON.stringify(team)
+  }).then(r => r.json());
 }
-
-function removeTeamRequest(id) {
-  fetch("http://localhost:3000/teams-json/delete", {
+function updateTeamRequest(team) {
+  return fetch("http://localhost:3000/teams-json/update", {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(team)
+  }).then(r => r.json());
+}
+function deleteTeamRequest(id) {
+  return fetch("http://localhost:3000/teams-json/delete", {
     method: "DELETE",
     headers: {
       "Content-Type": "application/json"
     },
-    body: JSON.stringify({ id: id })
-  });
+    body: JSON.stringify({ id })
+  }).then(r => r.json());
+}
+function readTeam() {
+  return {
+    promotion: document.getElementById("promotion").value,
+    members: document.getElementById("members").value,
+    name: document.getElementById("name").value,
+    url: document.getElementById("url").value
+  };
 }
 
+function getTeamsHTML(teams) {
+  return teams
+    .map(
+      team => `
+              <tr>
+        <td>${team.promotion}</td>
+        <td>${team.members}</td>
+        <td>${team.name}</td>
+        <td>
+        <a href = "${team.url}" target = "_blank">${team.url.replace("https://github.com/", "")}<a>
+        </td>
+        <td>
+          <a data-id="${team.id}" class="remove-btn">✖</a>
+          <a data-id="${team.id}" class="edit-btn">&#9998;</a>
+        </td>
+      </tr>`
+    )
+
+    .join("");
+}
+
+function displayTeams(teams) {
+  document.querySelector("#teams tbody").innerHTML = getTeamsHTML(teams);
+}
+
+function onSubmit(e) {
+  e.preventDefault();
+  const team = readTeam();
+  if (editId) {
+    team.id = editId;
+    updateTeamRequest(team).then(status => {
+      if (status.success) {
+        window.location.reload();
+      }
+    });
+  } else {
+    createTeamRequest(team).then(status => {
+      if (status.success) {
+        window.location.reload();
+      }
+    });
+  }
+}
+function prepareEdit(id) {
+  const team = allTeams.find(team => team.id === id);
+  editId = id;
+  document.getElementById("promotion").value = team.promotion;
+  document.getElementById("members").value = team.members;
+  document.getElementById("name").value = team.name;
+  document.getElementById("url").value = team.url;
+}
 function initEvents() {
   const form = document.getElementById("editForm");
   form.addEventListener("submit", onSubmit);
+  document.querySelector("#teams tbody").addEventListener("click", e => {
+    if (e.target.matches("a.remove-btn")) {
+      const id = e.target.dataset.id;
+      deleteTeamRequest(id).then(status => {
+        if (status.success) {
+          window.location.reload();
+        }
+      });
+    } else if (e.target.matches("a.edit-btn")) {
+      const id = e.target.dataset.id;
+      prepareEdit(id);
+    }
+  });
 }
 initEvents();
-
-console.warn("start app");
